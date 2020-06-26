f3e3e8c440317426d57d79b9157507a3762fd2db
wassim-code <ghachiwassim200@gmail.com>
2020-06-26 18:35:01 +0100

    Updated settings

--- developersask/settings.py
+++ developersask/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o12&8s04$fg%6e-z%_88f562ol*en)nbbcg7r@86-qm_f*+vd+'
+#SECRET_KEY = 'o12&8s04$fg%6e-z%_88f562ol*en)nbbcg7r@86-qm_f*+vd+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['developersask.herokuapp.com']
 
 AUTH_USER_MODEL = 'accounts.User'
 
